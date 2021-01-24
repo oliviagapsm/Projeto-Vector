8edcfaf19c65c8c85ee86ef41b4be62d60cfa47d
oliviagapsm <77942819+oliviagapsm@users.noreply.github.com>
2021-01-24 20:22:42 -0300

    Adicionado AnimalsAkinatorClient

---
+++ AnimalsAkinatorClient/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormularioComponent } from './paginas/formulario/formulario.component';
+import { PrincipalComponent } from './paginas/principal/principal.component';
+
+const routes: Routes = [
+  { path: '', component: PrincipalComponent},
+  { path: 'formulario', component: FormularioComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
